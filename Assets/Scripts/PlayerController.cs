b94f6ac8528c91e11ef0a6cf2a9e1abf0cd8fd3c
Aleks Ramach <aleks.ramach@teb.edu.pl>
2024-02-12 09:32:30 +0100

    testowe wypchniecie 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   //dodaj do wspó³rzêdnych wartoœæ x=1,y=0 z=0 pomno¿one przez czas w s 
-        // mierzony w sekundach od ostatniej klatki //test 
+        // mierzony w sekundach od ostatniej klatki //test //teseeee
         transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
     }
 }
